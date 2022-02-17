c472607e003bf2a95a6f7a2afcd90626c198e19f
LoKi <sand5088@gmail.com>
2022-02-17 10:52:18 +0700

    Add boostrap with Rails 7

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,11 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+import "bootstrap"
+
+//= require jquery
+//= require rails-ujs
+//= require turbolinks
+//= require bootstrap-sprockets
+//= require_tree .
